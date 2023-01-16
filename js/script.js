34a048a70b7012dd6baffaa3bcf5a7b3b404d3b1
sulek mulay-kulkarni <sulekmulay@gmail.com>
2023-01-16 12:56:12 -0800

    Update script.js

--- js/script.js
+++ js/script.js
@@ -137,6 +137,7 @@ function chooseRandomCategory (categories) {
   // 'categoryShort' is a short_name for a category
   dc.loadMenuItems = function (categoryShort) {
     showLoading("#main-content");
+    console.log("menuitemsURL...."+menuItemsUrl + categoryShort + ".json");
     $ajaxUtils.sendGetRequest(
       menuItemsUrl + categoryShort + ".json",
       buildAndShowMenuItemsHTML
